88bfea04c3b388a00efb184efb52ea08e24961cf
dimas7git <77691397+dimas7git@users.noreply.github.com>
2023-09-19 10:34:09 -0300

    aula04-android

--- AplicacaoMobile-/ex02soma/app/src/main/java/com/example/ex02_soma/MainActivity.java
+++ AplicacaoMobile-/ex02soma/app/src/main/java/com/example/ex02_soma/MainActivity.java
@@ -27,4 +27,37 @@ public class MainActivity extends AppCompatActivity {
         res = n1+n2;
         resultado.setText(""+res);
     }
+    public void dividir(View view){
+        TextView resultado = findViewById(R.id.resultado);
+        EditText numero1 = findViewById(R.id.editText);
+        EditText numero2 = findViewById(R.id.editText2);
+        //vamos criar variavies numericas para receber o valor convertido da String
+        double n1,n2,res;
+        n1=Integer.parseInt(numero1.getText().toString());
+        n2=Integer.parseInt(numero2.getText().toString());
+        res = n1/n2;
+        resultado.setText(""+res);
+    }
+    public void subtrair(View view){
+        TextView resultado = findViewById(R.id.resultado);
+        EditText numero1 = findViewById(R.id.editText);
+        EditText numero2 = findViewById(R.id.editText2);
+        //vamos criar variavies numericas para receber o valor convertido da String
+        double n1,n2,res;
+        n1=Integer.parseInt(numero1.getText().toString());
+        n2=Integer.parseInt(numero2.getText().toString());
+        res = n1-n2;
+        resultado.setText(""+res);
+    }
+    public void multiplicar(View view){
+        TextView resultado = findViewById(R.id.resultado);
+        EditText numero1 = findViewById(R.id.editText);
+        EditText numero2 = findViewById(R.id.editText2);
+        //vamos criar variavies numericas para receber o valor convertido da String
+        double n1,n2,res;
+        n1=Integer.parseInt(numero1.getText().toString());
+        n2=Integer.parseInt(numero2.getText().toString());
+        res = n1*n2;
+        resultado.setText(""+res);
+    }
 }
