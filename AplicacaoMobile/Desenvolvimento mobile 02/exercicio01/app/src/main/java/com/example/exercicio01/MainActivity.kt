f3969c9d1d7c891eaaebed61922e9642ba49ad86
dimas7git <77691397+dimas7git@users.noreply.github.com>
2024-03-22 12:03:29 -0300

    update

---
+++ AplicacaoMobile/Desenvolvimento mobile 02/exercicio01/app/src/main/java/com/example/exercicio01/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.exercicio01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
