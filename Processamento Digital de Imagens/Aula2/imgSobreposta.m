598808a270fd8c0dc495736994fdeec3082cd41b
dimas7git <dimaslimaferreira@gmail.com>
2024-09-18 11:52:32 -0300

    tarefa feita

--- Processamento Digital de Imagens/Aula2/imgSobreposta.m
+++ Processamento Digital de Imagens/Aula2/imgSobreposta.m
@@ -3,18 +3,21 @@ clc;
 
 imgMaior = imread('Lena512.bmp');
 imgMenor = imread('if30.bmp');
-imgMenor1 = rgb2gray(imgMenor);
 
-[M,N] = size(imgMenor);
+
+[M, N] = size(imgMenor);
+
 imgNova = imgMaior;
 
-for i=1:M
-  for j = 1:N+512
-    imgNova(i,j) = (imgMenor(i,j));
-  endfor
-endfor
 
-figure(1), subplot(1,3,1),imshow(imgMaior);
-figure(1), subplot(1,3,2),imshow(imgMenor);
-figure(1), subplot(1,3,3),imshow(imgNova);
+for i = 1:M
+  for j = 1:N
+    imgNova(end - M + i, end - N + j) = imgMenor(i, j);
+  end
+end
+
+
+figure(1),imshow(imgNova);
+
 imwrite(imgNova, 'lenaif.bmp');
+
