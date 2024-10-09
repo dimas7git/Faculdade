e1d803b4705acea84aa5e8990e3b9e17d7e3fb63
dimas7git <77691397+dimas7git@users.noreply.github.com>
2024-10-09 08:29:17 -0300

    filtro detectar borda

---
+++ Processamento Digital de Imagens/Aula5/imgFiltroBorda.m
@@ -0,0 +1,21 @@
+pkg load image;
+clc;
+
+[filename, pathname] = uigetfile('Selecione a imagem');
+[img] = imread(filename);
+
+imgSobel = edge(img,'sobel');
+imgCanny = edge(img,'canny',0.1);
+imgRoberts = edge(img,'roberts',0.3);
+imgPrewitt = edge(img,'prewitt');
+
+imwrite(imgSobel,'ImagemSobe1.bmp');
+imwrite(imgCanny,'ImagemCanny.bmp');
+imwrite(imgRoberts,'ImagemRoberts.bmp');
+imwrite(imgPrewitt,'Imagem prewiit.bmp');
+
+figure(1),subplot(1,4,1), imshow(imgSobel);
+figure(1),subplot(1,4,2), imshow(imgCanny);
+figure(1),subplot(1,4,3), imshow(imgRoberts);
+figure(1),subplot(1,4,4), imshow(imgPrewitt);
+
